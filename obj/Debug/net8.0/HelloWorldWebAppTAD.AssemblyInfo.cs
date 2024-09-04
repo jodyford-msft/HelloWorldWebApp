d74cb5772ec52abd9265687995d477a4d0092aa5
Jody Ford <jodyford@microsoft.com>
2024-09-04 11:52:34 -0400

    updated for sd

--- obj/Debug/net8.0/HelloWorldWebAppTAD.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorldWebAppTAD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldWebAppTAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949cca176668dbf5668dcf35a844ecb1961a368f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee51d15544d1839bddd7fe555f5637b900cd3ae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldWebAppTAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldWebAppTAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
